238f2055c15a52c7a04ad2939f8d2b53714398fb
DariaForost <dasha.forostenko@gmail.com>
2023-03-15 12:45:59 +0200

    Edit HomeWork4

--- src/parallelepiped/MyApp.java
+++ src/parallelepiped/MyApp.java
@@ -7,11 +7,7 @@ public class MyApp {
         int volume = ad * ab * ae;
         System.out.println("Volume of parallelepiped = " + volume);
 
-        int length1 = (ad * 2) + (ae * 2);
-        int length2 = (ab * 2) + (ae * 2);
-        int length3 = (ab * 2) + (ad * 2);
-        int length;
-        length = (length1 * 2) + (length2 * 2) + (length3 * 2);
+        int length = 4 * (ad + ab + ae);
         System.out.println("Sum of lengths = " + length);
     }
 }
