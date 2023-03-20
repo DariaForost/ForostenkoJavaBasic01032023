09b1ad4bff9d8f5392ee60ce034dac1aaa492ae1
DariaForost <dasha.forostenko@gmail.com>
2023-03-20 03:26:40 +0200

    Optimization

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,5 +1,12 @@
 public class MyClass {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        int num1 = 10;
+        int num2 = 5;
+        int sum = 0;
+
+        sum += num1;
+        sum -= num2;
+
+        System.out.println("sum after adding num1: " + sum);
     }
 }
--- src/coordinates/MyApp.java
+++
@@ -1,9 +0,0 @@
-package coordinates;
-public class MyApp {
-    public static void main(String[] args) {
-        double myLatitude = 49.446542585456254;
-        double myLongitude = 31.277485445845458;
-        System.out.println(myLatitude);
-        System.out.println(myLongitude);
-    }
-}
